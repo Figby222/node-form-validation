0013f1d7d6c96331103ccf2eefe065c1bf22d5f6
Figby222 <figby222@outlook.com>
2024-09-03 23:24:39 -0700

    Add validation controller to usersCreatePost

--- controllers/usersController.mjs
+++ controllers/usersController.mjs
@@ -1,4 +1,17 @@
 import usersStorage from "../storages/usersStorage.mjs";
+import { body, validationResult } from "express-validator";
+
+const alphaErr = "must only contain letters.";
+const lengthErr = "must be between 1 and 10 characters";
+
+const validateUser = [
+    body("firstName").trim()
+        .isAlpha().withMessage(`First name ${alphaErr}`)
+        .isLength({ min: 1, max: 10 }).withMessage(`First name ${lengthErr}`),
+    body("lastName").trim()
+    .isAlpha().withMessage(`Last name ${alphaErr}`)
+    .isLength({ min: 1, max: 10 }).withMessage(`Last name ${lengthErr}`),
+];
 
 function usersListGet(req, res) {
     res.render("users", {
@@ -13,10 +26,21 @@ function usersCreateGet(req, res) {
     })
 }
 
-function usersCreatePost(req, res) {
-    const { firstName, lastName } = req.body;
-    usersStorage.addUser({ firstName, lastName });
-    res.redirect("/users");
-}
+const usersCreatePost = [
+    validateUser,
+    (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).render("createUser", {
+                title: "Create user",
+                errors: errors.array(),
+            });
+        }
+        const { firstName, lastName } = req.body;
+        usersStorage.addUser({ firstName, lastName });
+        res.redirect("/users");
+    }
+]
+
 
 export { usersListGet, usersCreateGet, usersCreatePost }
