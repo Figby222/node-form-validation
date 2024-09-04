faa79ce1f6117f38a8ff019bf52d07a37e5d08f9
Figby222 <figby222@outlook.com>
2024-09-03 20:43:06 -0700

    Use express urlencoded middleware

--- index.mjs
+++ index.mjs
@@ -11,6 +11,8 @@ app.set("views", path.join(__dirname, "views"));
 
 app.set("view engine", "ejs");
 
+app.use(express.urlencoded({ extended: true }));
+
 app.use("/", indexRouter);
 
 app.listen(process.env.PORT, () => {
