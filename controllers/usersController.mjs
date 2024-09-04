a2a5147f9e5ca7fe673e17f8495012efb9bf919f
Figby222 <figby222@outlook.com>
2024-09-04 00:54:28 -0700

    Add email, age, & bio fields with validation

--- controllers/usersController.mjs
+++ controllers/usersController.mjs
@@ -3,14 +3,25 @@ import { body, validationResult } from "express-validator";
 
 const alphaErr = "must only contain letters.";
 const lengthErr = "must be between 1 and 10 characters";
+const emailFormatErr = "must be formatted like so: example@example.com";
+const ageRangeErr = "Must be a number between 18 and 120";
+const bioLengthErr = "Must be 200 or less characters";
 
 const validateUser = [
     body("firstName").trim()
         .isAlpha().withMessage(`First name ${alphaErr}`)
         .isLength({ min: 1, max: 10 }).withMessage(`First name ${lengthErr}`),
     body("lastName").trim()
-    .isAlpha().withMessage(`Last name ${alphaErr}`)
-    .isLength({ min: 1, max: 10 }).withMessage(`Last name ${lengthErr}`),
+        .isAlpha().withMessage(`Last name ${alphaErr}`)
+        .isLength({ min: 1, max: 10 }).withMessage(`Last name ${lengthErr}`),
+    body("email").trim()
+        .isEmail().withMessage(`Email ${emailFormatErr}`),
+    body("age").trim()
+        .optional()
+        .isInt({ min: 18, max: 120 }).withMessage(`Age ${ageRangeErr}`),
+    body("bio").trim()
+        .optional()
+        .isLength({ max: 200 }).withMessage(`Bio ${bioLengthErr}`),
 ];
 
 function usersListGet(req, res) {
@@ -36,8 +47,8 @@ const usersCreatePost = [
                 errors: errors.array(),
             });
         }
-        const { firstName, lastName } = req.body;
-        usersStorage.addUser({ firstName, lastName });
+        const { firstName, lastName, email, age, bio } = req.body;
+        usersStorage.addUser({ firstName, lastName, email, age, bio });
         res.redirect("/users");
     }
 ]
@@ -62,8 +73,8 @@ const usersUpdatePost = [
                 errors: errors.array(),
             });
         }
-        const { firstName, lastName } = req.body;
-        usersStorage.updateUser(req.params.id, { firstName, lastName });
+        const { firstName, lastName, email, age, bio } = req.body;
+        usersStorage.updateUser(req.params.id, { firstName, lastName, email, age, bio });
         res.redirect("/users");
     }
 ]
