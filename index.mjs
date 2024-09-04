25544c147384c9f098579569ed4b65c2365e068f
Figby222 <figby222@outlook.com>
2024-09-03 20:17:54 -0700

    Add index.mjs

---
+++ index.mjs
@@ -0,0 +1,18 @@
+import express from "express";
+import indexRouter from "./routers/indexRouter.mjs";
+import path from "node:path";
+import "dotenv/config";
+
+const app = express();
+
+const __dirname = import.meta.dirname;
+
+app.set("views", path.join(__dirname, "views"));
+
+app.set("view engine", "ejs");
+
+app.use("/", indexRouter);
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server listening on port ${process.env.PORT}`)
+})
